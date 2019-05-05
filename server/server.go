e2c8c6a1179ca1ab3d100df086fba27a2fa1e674
jbpratt78 <jbpratt78@gmail.com>
2019-05-05 18:56:18 -0500

    initial data seeded

--- server/server.go
+++ server/server.go
@@ -27,9 +27,16 @@ type server struct {
 	db *sqlx.DB
 }
 
-func (*server) GetMenu(ctx context.Context, empty *empty.Empty) (*mookiespb.Menu, error) {
+func (s *server) GetMenu(ctx context.Context, empty *empty.Empty) (*mookiespb.Menu, error) {
 	log.Println("Menu function was invoked")
-	res := &mookiespb.Menu{}
+	var categories []*mookiespb.Category
+	err := s.db.Select(&categories, "SELECT * from categories")
+	if err != nil {
+		return nil, err
+	}
+	res := &mookiespb.Menu{
+		Categories: categories,
+	}
 	return res, nil
 }
 
@@ -105,6 +112,7 @@ func NewServer(db *sqlx.DB) *server {
 }
 
 func main() {
+	seedData()
 	flag.Parse()
 	lis, err := net.Listen("tcp", *listen)
 	if err != nil {
@@ -128,11 +136,6 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
-
-	err = server.seedData()
-	if err != nil {
-		log.Fatal("Failed to seed data: %v\n", err)
-	}
 }
 
 const itemDbSchema = `
@@ -140,6 +143,10 @@ CREATE TABLE IF NOT EXISTS items (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL,
 	price DECIMAL NOT NULL
+	category_id INTEGER,
+	CONSTRAINT fk_categories
+		FOREIGN KEY (category_id)
+		REFERENCES categories(id)
 );`
 
 const categoryDbSchema = `
@@ -148,98 +155,102 @@ CREATE TABLE IF NOT EXISTS categories (
 	name TEXT NOT NULL
 );`
 
-func (s *server) seedData() error {
+func seedData() {
 	data := []*mookiespb.Category{
 		{
 			Name: "Sandwich",
 			Items: []*mookiespb.Item{
-				{Name: "Large Smoked Pulled Pork", Id: 1, Price: 495},
-				{Name: "Regular Smoked Pulled Pork", Id: 2, Price: 395},
-				{Name: "Large Smoked Chicken Breast", Id: 3, Price: 495},
-				{Name: "Regular Smoked Chicken Breast", Id: 4, Price: 395},
-				{Name: "'The Molly'", Id: 5, Price: 395},
-				{Name: "Large Hamburger", Id: 6, Price: 495},
-				{Name: "Hamburger", Id: 7, Price: 395},
-				{Name: "Large Cheeseburger", Id: 8, Price: 550},
-				{Name: "Cheeseburger", Id: 9, Price: 425},
-				{Name: "Grilled Cheese", Id: 10, Price: 300},
-				{Name: "Pulled Pork Melt", Id: 11, Price: 395},
+				{Name: "Large Smoked Pulled Pork", Id: 1, Price: 495, CategoryID: 1},
+				{Name: "Regular Smoked Pulled Pork", Id: 2, Price: 395, CategoryID: 1},
+				{Name: "Large Smoked Chicken Breast", Id: 3, Price: 495, CategoryID: 1},
+				{Name: "Regular Smoked Chicken Breast", Id: 4, Price: 395, CategoryID: 1},
+				{Name: "'The Molly'", Id: 5, Price: 395, CategoryID: 1},
+				{Name: "Large Hamburger", Id: 6, Price: 495, CategoryID: 1},
+				{Name: "Hamburger", Id: 7, Price: 395, CategoryID: 1},
+				{Name: "Large Cheeseburger", Id: 8, Price: 550, CategoryID: 1},
+				{Name: "Cheeseburger", Id: 9, Price: 425, CategoryID: 1},
+				{Name: "Grilled Cheese", Id: 10, Price: 300, CategoryID: 1},
+				{Name: "Pulled Pork Melt", Id: 11, Price: 395, CategoryID: 1},
 			},
 		},
 		{
 			Name: "Plates",
 			Items: []*mookiespb.Item{
-				{Name: "Smoked Pulled Pork", Id: 12, Price: 990},
-				{Name: "Regular Rib", Id: 13, Price: 995},
-				{Name: "1/2 Smoked Chicken", Id: 14, Price: 995},
-				{Name: "Smoked Chicken Breast", Id: 15, Price: 725},
-				{Name: "Smoked Wing (8 wings)", Id: 16, Price: 999},
-				{Name: "Loaded Nachos (BBQ or Chicken)", Id: 17, Price: 875},
+				{Name: "Smoked Pulled Pork", Id: 12, Price: 990, CategoryID: 2},
+				{Name: "Regular Rib", Id: 13, Price: 995, CategoryID: 2},
+				{Name: "1/2 Smoked Chicken", Id: 14, Price: 995, CategoryID: 2},
+				{Name: "Smoked Chicken Breast", Id: 15, Price: 725, CategoryID: 2},
+				{Name: "Smoked Wing (8 wings)", Id: 16, Price: 999, CategoryID: 2},
+				{Name: "Loaded Nachos (BBQ or Chicken)", Id: 17, Price: 875, CategoryID: 2},
 			},
 		},
 		{
 			Name: "Baskets",
 			Items: []*mookiespb.Item{
-				{Name: "Smoked Wing", Id: 18, Price: 500},
-				{Name: "Rib", Id: 19, Price: 500},
+				{Name: "Smoked Wing", Id: 18, Price: 500, CategoryID: 3},
+				{Name: "Rib", Id: 19, Price: 500, CategoryID: 3},
 			},
 		},
 		{
 			Name: "Potatoes",
 			Items: []*mookiespb.Item{
-				{Name: "Loaded Pork", Id: 20, Price: 899},
-				{Name: "Loaded Chicken", Id: 21, Price: 899},
-				{Name: "Loaded Potato (no meat)", Id: 22, Price: 699},
-				{Name: "Smothered and Covered Fries", Id: 23, Price: 899},
+				{Name: "Loaded Pork", Id: 20, Price: 899, CategoryID: 4},
+				{Name: "Loaded Chicken", Id: 21, Price: 899, CategoryID: 4},
+				{Name: "Loaded Potato (no meat)", Id: 22, Price: 699, CategoryID: 4},
+				{Name: "Smothered and Covered Fries", Id: 23, Price: 899, CategoryID: 4},
 			},
 		},
 		{
 			Name: "Sides",
 			Items: []*mookiespb.Item{
-				{Name: "Small Cole Slaw", Id: 24, Price: 300},
-				{Name: "Large Cole Slaw", Id: 25, Price: 600},
-				{Name: "Small Baked Beans", Id: 26, Price: 300},
-				{Name: "Large Baked Beans", Id: 27, Price: 600},
-				{Name: "Small Potato Salad", Id: 28, Price: 300},
-				{Name: "Large Potato Salad", Id: 29, Price: 600},
-				{Name: "Plain Chips", Id: 30, Price: 100},
-				{Name: "Fries", Id: 31, Price: 300},
+				{Name: "Small Cole Slaw", Id: 24, Price: 300, CategoryID: 5},
+				{Name: "Large Cole Slaw", Id: 25, Price: 600, CategoryID: 5},
+				{Name: "Small Baked Beans", Id: 26, Price: 300, CategoryID: 5},
+				{Name: "Large Baked Beans", Id: 27, Price: 600, CategoryID: 5},
+				{Name: "Small Potato Salad", Id: 28, Price: 300, CategoryID: 5},
+				{Name: "Large Potato Salad", Id: 29, Price: 600, CategoryID: 5},
+				{Name: "Plain Chips", Id: 30, Price: 100, CategoryID: 5},
+				{Name: "Fries", Id: 31, Price: 300, CategoryID: 5},
 			},
 		},
 		{
 			Name: "Drinks",
 			Items: []*mookiespb.Item{
-				{Name: "Canned Drink", Id: 32, Price: 100},
-				{Name: "Bottled Water", Id: 33, Price: 150},
+				{Name: "Canned Drink", Id: 32, Price: 100, CategoryID: 6},
+				{Name: "Bottled Water", Id: 33, Price: 150, CategoryID: 6},
 			},
 		},
 		{
 			Name: "Desserts",
 			Items: []*mookiespb.Item{
-				{Name: "Oreo Dream", Id: 34, Price: 350},
-				{Name: "Lemon Delight", Id: 35, Price: 350},
-				{Name: "Strawberry Pizza", Id: 36, Price: 350},
-				{Name: "Whole Dessert", Id: 37, Price: 3000},
-				{Name: "Small Banana Pudding", Id: 38, Price: 350},
-				{Name: "Large Banana Pudding", Id: 39, Price: 700},
+				{Name: "Oreo Dream", Id: 34, Price: 350, CategoryID: 7},
+				{Name: "Lemon Delight", Id: 35, Price: 350, CategoryID: 7},
+				{Name: "Strawberry Pizza", Id: 36, Price: 350, CategoryID: 7},
+				{Name: "Whole Dessert", Id: 37, Price: 3000, CategoryID: 7},
+				{Name: "Small Banana Pudding", Id: 38, Price: 350, CategoryID: 7},
+				{Name: "Large Banana Pudding", Id: 39, Price: 700, CategoryID: 7},
 			},
 		},
 		{
 			Name: "Sauces",
 			Items: []*mookiespb.Item{
-				{Name: "Extra sauce", Id: 40, Price: 50},
+				{Name: "Extra sauce", Id: 40, Price: 50, CategoryID: 8},
 			},
 		},
 	}
 
 	for _, c := range data {
-		t := "INSERT INTO categories (name) VALUES ('%s')"
+		t := "INSERT INTO categories (name) VALUES ('%s');"
 		cmd := fmt.Sprintf(t, c.GetName())
 		fmt.Println(cmd)
-		res := s.db.MustExec(cmd)
-		fmt.Println(res)
+		//res := s.db.MustExec(cmd)
 	}
 
-	fmt.Println("Data has been seeded")
-	return nil
+	for _, c := range data {
+		for _, i := range c.GetItems() {
+			t := "INSERT INTO items (name, price, category_id) VALUES ('%v', '%v', '%v');"
+			cmd := fmt.Sprintf(t, i.GetName(), i.GetPrice(), i.GetCategoryID())
+			fmt.Println(cmd)
+		}
+	}
 }
