174a47b0c6a605cee841d854f9ed0f28526e7905
jbpratt78 <jbpratt78@gmail.com>
2019-06-21 12:39:21 -0500

    Add max conn idle to server keep alive params
    https://stackoverflow.com/questions/52993259/problem-with-grpc-setup-getting-an-intermittent-rpc-unavailable-error/54703234#54703234

--- server/server.go
+++ server/server.go
@@ -28,13 +28,16 @@ const topicComplete = "complete"
 var (
 	reg         = prometheus.NewRegistry()
 	grpcMetrics = grpc_prometheus.NewServerMetrics()
-	kasp        = keepalive.ServerParameters{Time: 5 * time.Second}
-	promAddr    = flag.String("prom", ":9001", "Port to run metrics HTTP server")
-	listen      = flag.String("listen", ":50051", "listen address")
-	dbp         = flag.String("database", "./mookies.db", "database to use")
-	tls         = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	crt         = flag.String("crt", "server.crt", "TLS cert to use")
-	key         = flag.String("key", "server.key", "TLS key to use")
+	kasp        = keepalive.ServerParameters{
+		Time:              5 * time.Second,
+		MaxConnectionIdle: 5 * time.Minute,
+	}
+	promAddr = flag.String("prom", ":9001", "Port to run metrics HTTP server")
+	listen   = flag.String("listen", ":50051", "listen address")
+	dbp      = flag.String("database", "./mookies.db", "database to use")
+	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	crt      = flag.String("crt", "server.crt", "TLS cert to use")
+	key      = flag.String("key", "server.key", "TLS key to use")
 )
 
 type server struct {
