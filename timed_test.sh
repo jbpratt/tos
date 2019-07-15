418b0c767a4a88f0af70e1003d525da68e72fe71
jbpratt78 <jbpratt78@gmail.com>
2019-07-14 19:10:20 -0500

    day test

--- timed_test.sh
+++ timed_test.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+i=0
 while true
 do
+  i=$((i + 1))
   go run cli/main.go -a :50051 order
-  echo "order placed"
-#sleep $((RANDOM % 350))
-sleep 60
+  echo "order placed $i"
+sleep $((RANDOM % 1200))
 done
