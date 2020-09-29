5b5461b5dda2f7573b9e713936c05ee98c6ffb38
jbpratt78 <jbpratt78@gmail.com>
2020-09-29 12:30:15 -0700

    generate swift pb

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -10,6 +10,7 @@ protoc "${PROTOFILE}" \
   --go_out "plugins=grpc:${OUT_DIR}" \
   --js_out "import_style=commonjs:${OUT_DIR}" \
   --grpc-web_out "import_style=commonjs,mode=grpcwebtext:${OUT_DIR}"
+  --swift_out="$OUT_DIR" --swift_opt Visibility=Public
 
 protoc-go-inject-tag -input="./protofiles/tos.pb.go"
 
