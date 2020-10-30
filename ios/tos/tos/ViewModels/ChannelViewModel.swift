0e6b9c855ffeb3ed9bf9c8869451fb034b79483f
jbpratt78 <jbpratt78@gmail.com>
2020-10-29 21:18:19 -0400

    swap to NIOTSEventLoopGroup for networking memes

--- ios/tos/tos/ViewModels/ChannelViewModel.swift
+++ ios/tos/tos/ViewModels/ChannelViewModel.swift
@@ -1,25 +1,47 @@
+import Combine
 import GRPC
+import Logging
 import NIO
+import NIOTransportServices
 
 class ChannelViewModel {
-    private let group = MultiThreadedEventLoopGroup(numberOfThreads: 2)
+    private let group = NIOTSEventLoopGroup()
     let conn: ClientConnection
+    let logger: Logger = Logger(label: "TOS")
 
     init() {
-        let conf = ClientConnection.Configuration(
+        var conf = ClientConnection.Configuration(
             target: .hostAndPort("localhost", 50051),
             eventLoopGroup: group,
+            connectionBackoff: nil,
             connectionKeepalive: ClientConnectionKeepalive(
-                interval: .seconds(15),
-                timeout: .seconds(10)
-            )
+                interval: .seconds(5),
+                timeout: .seconds(3)
+            ),
+            backgroundActivityLogger: self.logger
         )
+        let delegate = RecordingDelegate()
+        conf.errorDelegate = delegate
+        conf.connectivityStateDelegate = delegate
         conn = ClientConnection(configuration: conf)
     }
 
     deinit {
         let _ = conn.close()
-        // this maybe should be defer'd
         try! group.syncShutdownGracefully()
+    }
+}
+
+class RecordingDelegate: ConnectivityStateDelegate, ClientErrorDelegate {
+    var errors: [Error] = []
+    var connectivity = CurrentValueSubject<ConnectivityState, Never>(.idle)
+    
+    func connectivityStateDidChange(from oldState: ConnectivityState, to newState: ConnectivityState) {
+        connectivity.value = newState
+    }
+    
+    func didCatchError(_ error: Error, logger: Logger, file: StaticString, line: Int) {
+        print(error)
+        errors.append(error)
     }
 }
--- ios/tos/tos/ViewModels/PingViewModel.swift
+++ ios/tos/tos/ViewModels/PingViewModel.swift
@@ -5,36 +5,31 @@ import GRPC
 class PingViewModel: ChannelViewModel, ObservableObject, Identifiable {
     private lazy var client = Tospb_PingServiceClient(channel: self.conn)
 
-    @Published var active: Bool = false
+    @Published private(set) var active: Bool = false
+    //@Published private(set) var connectivity: ConnectivityState = .idle
 
     override init() {
         super.init()
+        self.ping()
         _ = Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { _ in
             self.ping()
         }
     }
     
     func ping() {
-        /*
-        let opts = CallOptions()
-        let req = Tospb_Ping.with {
-            $0.message = "ping"
-        }
-        let call = pingClient.sendPing(req, callOptions: opts)
-         */
-        //DispatchQueue.main.async {
         self.client.sendPing(Tospb_Ping.with {
             $0.message = "ping"
         }).response.whenComplete { res in
-            print(res)
-            switch res {
-            case .success(_):
-                self.active = true
-            case .failure(let err):
-                self.active = false
-                print("sendPing failed: \(err)")
+            DispatchQueue.main.async {
+                switch res {
+                case .success(_):
+                    self.active = true
+                    print("sendPing success: \(res)")
+                case .failure(let err):
+                    self.active = false
+                    print("sendPing failed: \(err)")
+                }
             }
         }
-        //}
     }
 }
